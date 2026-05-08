ed218dd81c10a088938f13d90f5781e24f8a4eec
Elisa Delaunay <delaunayelisa97@gmail.com>
2026-05-08 15:30:07 +0200

    Test push main

--- frontend/src/components/Layout/AuthLayout/AuthLayout.jsx
+++ frontend/src/components/Layout/AuthLayout/AuthLayout.jsx
@@ -3,7 +3,7 @@
 export default function AuthLayout() {
     return (
         <div className="auth-layout-container">
-            {/*Mettre ici la user card + nav card + librairie card*/}
+            {/*Mettre ici la user card + nav card + librairie card  */}
             <header>Resonate - Bienvenue (version user connecté)</header>
 
             <main className="auth-main-content">
