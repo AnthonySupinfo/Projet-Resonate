079136e21575dbccae97ef6b543633c42bea4255
Elisa Delaunay <delaunayelisa97@gmail.com>
2026-05-09 19:14:51 +0200

    Model feed

---
+++ backend/app/models/user_activity_feed.py
@@ -0,0 +1,31 @@
+﻿import enum
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Enum as SQLEnum
+from sqlalchemy.sql import func
+from app.db.session import Base
+
+
+class ActivityTypes(str, enum.Enum):
+    LIKE_TRACK = "LIKE_TRACK"
+    LIKE_ALBUM = "LIKE_ALBUM"
+    FOLLOW_PLAYLIST = "FOLLOW_PLAYLIST"
+    FOLLOW_USER = "FOLLOW_USER"
+    ADD_TRACK_PLAYLIST = "ADD_TRACK_PLAYLIST"
+    REVIEW_ALBUM = "REVIEW_ALBUM"
+
+
+class UserActivityFeed(Base):
+    __tablename__ = "user_activity_feed"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(String, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    activity_type = Column(SQLEnum(ActivityTypes), nullable=False)
+
+    target_user_id = Column(String, ForeignKey("users.id", ondelete="CASCADE"), nullable=True)
+    review_id = Column(Integer, ForeignKey("reviews.id", ondelete="CASCADE"), nullable=True)
+    playlist_id = Column(Integer, ForeignKey("playlists.id", ondelete="CASCADE"), nullable=True)
+    album_id = Column(Integer, ForeignKey("albums.id", ondelete="CASCADE"), nullable=True)
+    track_id = Column(Integer, ForeignKey("tracks.id", ondelete="CASCADE"), nullable=True)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
