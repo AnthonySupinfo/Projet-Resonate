038b58d90bc7cbc8a03bd80517fe0348d8510cdb
Elisa Delaunay <delaunayelisa97@gmail.com>
2026-05-10 15:30:12 +0200

    user card + mise en place variables css

--- frontend/src/components/Layout/AuthLayout/AuthLayout.jsx
+++ frontend/src/components/Layout/AuthLayout/AuthLayout.jsx
@@ -1,15 +1,17 @@
 ﻿import { Outlet } from "react-router-dom";
 import './AuthLayout.css';
 import NavCard from "../../Shared/NavCard/NavCard.jsx";
+import UserCard from "../../Shared/UserCard/UserCard.jsx";
+import LibraryCard from "../../library/libraryCard/LibraryCard.jsx";
 import LanguageSwitch from "../../Shared/LanguageSwitch/LanguageSwitch.jsx";
 
 export default function AuthLayout() {
     return (
         <div className="auth-layout">
             <aside>
-                <LogoCard />
+                <UserCard />
                 <NavCard />
-                <LanguageSwitch />
+                <LibraryCard />
             </aside>
 
             <main className="auth-content">
