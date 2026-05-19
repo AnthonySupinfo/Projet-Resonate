821423cf213033f6c8021baf71390d55c280e743
Elisa Delaunay <delaunayelisa97@gmail.com>
2026-05-19 18:29:35 +0200

    Messages et conversations : models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -12,8 +12,9 @@ from .user_album_status import UserAlbumStatus
 from .user_playlist_item import UserPlaylistItem
 from .user_playlist_status import UserPlaylistStatus
 from .user_activity_feed import UserActivityFeed
+from .message import Conversation, Message
 
-__all__ = ["Base", "User", "Album", "Follow", "OAuthAccount", "UserActivityFeed"]
+__all__ = ["Base", "User", "Album", "Follow", "OAuthAccount", "UserActivityFeed", "Conversation", "Message"]
 from app.models.album import Album
 from app.models.artist import Artist
 from app.models.track import Track
