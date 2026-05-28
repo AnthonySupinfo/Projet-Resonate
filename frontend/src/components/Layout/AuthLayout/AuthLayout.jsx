b200210548d043ad4ca6d2cf12992bb6a750e0e6
Elisa Delaunay <delaunayelisa97@gmail.com>
2026-05-28 18:11:49 +0200

    Layout OK

--- frontend/src/components/Layout/AuthLayout/AuthLayout.jsx
+++ frontend/src/components/Layout/AuthLayout/AuthLayout.jsx
@@ -1,49 +1,46 @@
-﻿import {Outlet, useLocation} from "react-router-dom";
+﻿import { Outlet, useLocation } from "react-router-dom";
 import './AuthLayout.css';
 import NavCard from "../../Shared/NavCard/NavCard.jsx";
 import UserCard from "../../Shared/UserCard/UserCard.jsx";
 import LibraryCard from "../../library/libraryCard/LibraryCard.jsx";
 import NotifsCard from "../../Shared/NotifsCard/NotifsCard.jsx";
-// TODO: importer SearchBar
-// import SearchBar from "../../Shared/SearchBar/SearchBar.jsx";
 
 export default function AuthLayout() {
     const location = useLocation();
+
     const isHomePage = location.pathname === '/';
-    const showFavPlaylist = location.pathname === '/' || location.pathname === '/social';
-    const showRightSidebar = !isProfilePage;
+    const hasRightSidebar = location.pathname === '/' || location.pathname === '/social';
 
     return (
-        <div className={`auth-layout ${isProfilePage ? 'profile-mode' : ''}`}>
+        <div className={`auth-layout ${!hasRightSidebar ? 'profile-mode' : ''} ${isHomePage ? 'home-mode' : ''}`}>
             <aside className="left-sidebar">
                 <UserCard />
                 <NavCard />
                 <LibraryCard />
             </aside>
 
-            <main className="auth-content">
+            <div className="auth-topbar">
                 {!isHomePage && (
-                    <div className="layout-topbar">
-                        {/* <SearchBar /> */}
-                        {/* ToDo: Placeholder à supprimer */}
-                        <div style={{ opacity: 0.5, padding: '12px 24px', background: 'rgba(255,255,255,0.4)', borderRadius: '24px', width: '400px', marginTop: '5px' }}>
-                            🔍 Que voulez-vous écouter ?
-                        </div>
+                    <div className="search-placeholder">
+                        🔍 Que voulez-vous écouter ?
                     </div>
                 )}
 
+                <div className="notifs-wrapper">
+                    <NotifsCard />
+                </div>
+            </div>
+
+            <main className="auth-content">
                 <Outlet />
             </main>
 
-            <aside>
-                <NotifsCard/>
-
-                {showFavPlaylist && (
+            {hasRightSidebar && (
+                <aside className="right-sidebar">
                     <div className="fav-playlist-placeholder">
-                        {/* <FavPlaylistCard /> */}
                     </div>
-                )}
-            </aside>
+                </aside>
+            )}
         </div>
     );
 }
